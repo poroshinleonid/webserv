16f791d7734d021ddbc60216b9aec8083f60d5ae
timkingteam <timkingteam@gmail.com>
2024-06-14 16:49:55 +0200

    python parsing

--- parsing/parser.py
+++ parsing/parser.py
@@ -6,6 +6,10 @@ LINKLIST: empty | LINK(,LINK)*
 STR: "some text"
 '''
 
+# put those 2 values in class / namespace to avoid globals
+value_found = ""
+key_to_find = "age"
+key_found = ""
 
 def remove_spaces(s):
     i = 0
@@ -23,7 +27,11 @@ def remove_spaces(s):
     return ''.join(res)
 
 def verify(s):
-    s = remove_spaces(s)
+    global key_found, value_found
+
+    key_found = ""
+    value_found = ""
+    s = remove_spaces(s) # move it outside to call it only once
     s = eat_obj(s)
     assert s == ''
 
@@ -54,33 +62,37 @@ def verify_linklist(s):
         s = s[1:]
 
 def eat_link(s):
-    s = eat_str(s)
+    global key_found
+
+    assert s and s[0] == '"'
+    i = s.find('"', 1)
+    assert i != -1
+    key_found = s[1:i]
+    s = s[i+1:]
     assert(s[0] == ':')
     s = eat_value(s[1:])
     return s
 
-def eat_str(s):
-    assert s and s[0] == '"'
-    i = 1
-    while i < len(s) and s[i] != '"':
-        i += 1
-    assert i < len(s)
-    return s[i+1:]
-
 def eat_value(s):
+    global value_found, key_found, key_to_find
+
     assert s
     if s[0] == '"':
-        return eat_str(s)
+        i = s.find('"', 1)
+        assert i != -1
+        if key_found == key_to_find:
+            value_found = s[1:i]
+        return s[i+1:]
     return eat_obj(s)
 
 if __name__ == '__main__':
     good = '''{
     "name": "John Doe",
-    "age": "30",
+    "street": "30",
     "email": "johndoe@example.com",
     "isEmployed": "true",
-    "address": {
-        "street": "123 Main St",
+    "age": {
+        "age": "123 Main St",
         "city": "Anytown",
         "state": "CA",
         "zip": "12345"
@@ -93,8 +105,7 @@ if __name__ == '__main__':
     "lastLogin": "2024-06-14T12:34:56Z"
 }'''
     verify(good)
-
-    import traceback
+    print(value_found)
     invalid_jsons = [
     '{"name": "Alice", "age": "25", "email": "alice@example.com"',
     "{'name': 'Bob', 'age': '30', 'email': 'bob@example.com'}",
