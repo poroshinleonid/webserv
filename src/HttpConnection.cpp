d3f70f675768ef271eddfc10e675532619848ecb
Leonid Poroshin <lporoshi@student.42heilbronn.de>
2024-07-11 16:09:24 +0200

    add HttpConnection constructor

--- src/HttpConnection.cpp
+++ src/HttpConnection.cpp
@@ -1,8 +1,29 @@
 #include "HttpConnection.hpp"
+#include "ServerConfig.hpp"
 
-HttpConnection::HttpConnection() {}
+HttpConnection::HttpConnection(Config &cfg) :config(cfg) {
+  (void)cfg;
+}
 
-HttpConnection::HttpConnection(HttpConnection const &other) : {
+HttpConnection::HttpConnection(int fd, ServerConfig &scfg, Config &cfg)  :
+    fd(fd), port(scfg.port), host(scfg.host), config(cfg),
+    recv_buffer(""), send_buffer(""),
+    busy(false), is_connected(true), 
+    is_cgi_running(false), 
+    cgi_response(""),
+    cgi_finished(false), should_die(false),
+    is_response_ready(false),
+    is_keep_alive(false), header_is_parsed(false), 
+    body_is_read(false), cgi_pid(0) {
+    last_activity = std::time(&last_activity);
+    (void)fd;
+    (void)scfg;
+    // config = cfg;
+    cgi_pipe[0] = 0;
+    cgi_pipe[1] = 0;
+}
+
+HttpConnection::HttpConnection(HttpConnection const &other) :config(other.config) {
   (void)other;
 };
 
--- src/HttpConnection.hpp
+++ src/HttpConnection.hpp
@@ -2,38 +2,58 @@
 #define HTTPCONNECTION_HPP
 
 #include "Config.hpp"
+#include "ServerConfig.hpp"
 
 #include <string>
 #include <map>
 #include <iostream>
 #include <vector>
 
+enum connection_state {
+  CON_WAIT,
+  CON_CGI_RUNNING,
+  CON_CGI_FIN,
+  CON_CGI_TIMEOUT,
+  CON_CGI_ERR,
+  CON_RECV_INCOMPL,
+  CON_RECV_COMPL,
+  CON_SEND_INCOMPL,
+  CON_ERR,
+  CON_CLOSED,
+};
+
 class	HttpConnection {
-private:
+public:
   HttpConnection();
   HttpConnection(HttpConnection const &other);
   ~HttpConnection();
   HttpConnection &operator=(const HttpConnection &obj);
 
 public:
+  HttpConnection(Config &cfg);
+  HttpConnection(int fd, ServerConfig &scfg, Config &cfg);
+
+
+public:
   int fd;
   int port;
+  std::string host;
   Config &config;
   std::string recv_buffer;
   std::string send_buffer;
   time_t last_activity;
   bool busy;
   bool is_connected;
-  bool has_forked;
+  bool is_cgi_running;
+  std::string cgi_response;
   bool cgi_finished;
   bool should_die;
   bool is_response_ready;
   bool is_keep_alive;
   bool header_is_parsed;
   bool body_is_read;
-  pid_t child_pid;
+  pid_t cgi_pid;
   int cgi_pipe[2];
-
 };
 
 #endif // HTTPCONNECTION_HPP
