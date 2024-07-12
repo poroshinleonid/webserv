ddb388e499a541fb2fce17deab10aaada34c7157
Leonid Poroshin <lporoshi@student.42heilbronn.de>
2024-07-12 17:54:15 +0200

    add HttpRequest and HttpResponse headers

--- src/HttpRequest.hpp
+++ src/HttpRequest.hpp
@@ -2,6 +2,19 @@
 #define HttpRequest_HPP
 
 #include <string>
+#include <map>
+
+enum http_method {
+  HTTP_METH_GET,
+  HTTP_METH_HEAD,
+  HTTP_METH_POST,
+  HTTP_METH_PUT,
+  HTTP_METH_DELETE,
+  HTTP_METH_CONNECT,
+  HTTP_METH_OPTIONS,
+  HTTP_METH_TRACE,
+  HTTP_METH_PATCH,
+};
 
 class	HttpRequest {
 public:
@@ -13,10 +26,35 @@ public:
 public:
   HttpRequest(const std::string &request_string);
 
+
+public:
+  /**
+   * @brief convert the whole request to a string
+   *
+   * @return std::string 
+   */
+  std::string to_string() const;
+
+
+// internal workings data
 public:
   bool is_valid;
   bool is_for_cgi;
-private:
+  std::string client_hostname;
+  std::string client_port;
+
+// Actual request fields
+public:
+  std::string header;
+  std::string body;
+  http_method method;
+  std::string uri;
+  std::string version;
+  std::map<std::string, std::string> header_params;
+  std::map<std::string, std::string> query_params;
+
+
+
 };
 
 #endif
--- src/HttpResponse.hpp
+++ src/HttpResponse.hpp
@@ -1,6 +1,11 @@
 #ifndef HttpResponse_HPP
 #define HttpResponse_HPP
 
+#include "HttpRequest.hpp"
+
+#include <string>
+#include <map>
+
 class	HttpResponse {
 public:
   HttpResponse();
@@ -8,7 +13,27 @@ public:
   ~HttpResponse();
   HttpResponse &operator=(const HttpResponse &obj);
 
-private:
+public:
+  std::string to_string() const;
+
+// internal workings data
+public:
+  bool is_valid;
+  bool is_for_cgi;
+  std::string client_hostname;
+  std::string client_port;
+
+// Actual request fields
+public:
+  int status_code;
+  std::string status_message;
+  std::string header;
+  std::string body;
+  http_method method;
+  std::string uri;
+  std::string version;
+  std::map<std::string, std::string> header_params;
+
 };
 
 #endif
