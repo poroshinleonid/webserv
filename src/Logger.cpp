525fde9c303a6bfc85fb1297da6fb2a1b17ee89a
a21mr <poroshin.leonid.a@gmail.com>
2024-07-19 16:35:22 +0200

    simpligy logger

--- src/Logger.cpp
+++ src/Logger.cpp
@@ -3,77 +3,44 @@
 #include <iostream>
 #include "LoggerStream.hpp"
 
-// deleted
-// Logger::Logger(Logger const &other) : {};
-// Logger	&Logger::operator=(const Logger &other) {
-//   if (this != &other) {
-//   }
-//   return (*this);
-// }
+Logger::Logger() : out_file_stream_(NULL);
 
-
-Logger::Logger() : debug("DEBUG"), info("INFO"), warning("WARNING"), error("ERROR"),
-                    debug_(NULL), info_(NULL), warning_(NULL), error_(NULL) {
-}
-
-Logger::Logger (const std::string &debug_file, const std::string &info_file,
-                const std::string &warning_file, const std::string &error_file)
-  : debug("DEBUG"), info("INFO"), warning("WARNING"), error("ERROR"),
-    debug_(NULL), info_(NULL), warning_(NULL), error_(NULL) {
-  debug_ = get_file_log_stream("DEBUG", debug_file);
-  info_ = get_file_log_stream("INFO", info_file);
-  warning_ = get_file_log_stream("WARNING", warning_file);
-  error_ = get_file_log_stream("ERROR", error_file);
-  if (debug_) {
-    debug = *debug_;
+Logger::Logger (const std::string &log_file) {
+  std::ofstream *out_stream = new out_stream;
+  out_stream->open(log_file.c_str(), ios_base::app);
+  if (out_stream->is_open()) {
+    out_file_stream_ = out_stream;
+    return ;
   }
-
+  out_file_stream_ = NULL;
+  delete out_stream;
+  return ;
 }
 
 Logger::~Logger() {
-  if (debug_) {
-    delete debug_;
-  }
-  if (info_) {
-    delete info_;
-  }
-  if (warning_) {
-    delete warning_;
-  }
-  if (error_) {
-    delete error_;
+  if (out_file_stream_ != NULL) {
+    delete out_file_stream_;
   }
 }
 
-LoggerStream *Logger::get_file_log_stream(const std::string &prefix, const std::string &filename) {
-  std::ofstream *file_stream = new std::ofstream;
-  file_stream->open(filename);
-  if (!file_stream->is_open()) {
-    std::cerr << "Could not open log file " << filename << std::endl;
-    delete file_stream;
-    return NULL;
-  }
-  LoggerStream *logger_stream = new LoggerStream(prefix, &std::clog, file_stream);
-  return logger_stream;
-}
-
-
-void Logger::log_warning(const std::string &log_lvl, const std::string &message) const {
+void Logger::log(const std::string &log_lvl, const std::string &message) const {
   std::cout << "[" << log_lvl << "] " << message << std::endl;
+  *out_file_stream_ << "[" << log_lvl << "] " << message << std::endl;
+
 }
 
 void Logger::log_warning(const std::string &message) const {
-  std::cout << "[WARNING] " << message << std::endl;
+  log("WARNING", message);
 }
 
 void Logger::log_error(const std::string &message) const {
-  std::cout << "[ERROR] " << message << std::endl;
+  log("ERROR", message);
 }
 
 void Logger::log_info(const std::string &message) const {
-  std::cout << "[INFO] " << message << std::endl;
+  log("INFO", message);
 }
 
 void Logger::log_debug(const std::string &message) const {
-  std::cout << "[DEBUG] " << message << std::endl;
+  log("DEBUG", message);
 }
--- src/Logger.hpp
+++ src/Logger.hpp
@@ -13,29 +13,19 @@ private:
 
 public:
   Logger();
-  Logger (const std::string &debug_file, const std::string &info_file,
-                const std::string &warning_file, const std::string &error_file);
+  Logger (const std::string &log_file);
   ~Logger();
 
-private:
-  LoggerStream *get_file_log_stream(const std::string &prefix, const std::string &filename);
 
-  void log_warning(const std::string &log_lvl, const std::string &message) const;
+public:
+  void log(const std::string &log_lvl, const std::string &message) const;
   void log_debug(const std::string &message) const;
   void log_info(const std::string &message) const;
   void log_warning(const std::string &message) const;
   void log_error(const std::string &message) const;
-public:
-  LoggerStream debug;
-  LoggerStream info;
-  LoggerStream warning;
-  LoggerStream error;
 
 private:
-  LoggerStream *debug_;
-  LoggerStream *info_;
-  LoggerStream *warning_;
-  LoggerStream *error_;
+  std::ofstream *out_file_stream_;
 };
 
 #endif // LOGGER_HPP
