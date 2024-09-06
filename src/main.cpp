6fd09294aa869517807329ee937bab73d8389274
Leonid Poroshin <lporoshi@student.42heilbronn.de>
2024-09-06 10:46:44 +0200

    misc sync

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,8 @@
 #include <map>
 #include <vector>
 
+#define DEBUG
+
 int main(int argc, char **argv){
   if (argc != 2) {
   std::cout << "gib args (conf file)\n";
@@ -17,21 +19,23 @@ int main(int argc, char **argv){
   std::map<std::string, std::string> dict;
   std::map<std::string, std::string>::iterator it;
 
-  v = config["dictionaries"].get_vec("dict");
-  for (size_t i = 0; i < v.size(); i++) {
-    std::cout << "DICT ITSELF: ";
-    std::cout << v[i].get_content() << std::endl;
-    dict = v[i].get_content_dict();
-    std::cout << "DICT CONTENT" << std::endl;
-    for (it = dict.begin(); it != dict.end(); it++) {
-      std::cout << it->first << ":" << it->second << ";" << std::endl;
-    }
-    std::cout << "----------" << std::endl;
-  }
-  return 0;
+  // v = config["dictionaries"].get_vec("dict");
+  // for (size_t i = 0; i < v.size(); i++) {
+  //   std::cout << "DICT ITSELF: ";
+  //   std::cout << v[i].get_content() << std::endl;
+  //   dict = v[i].get_content_dict();
+  //   std::cout << "DICT CONTENT" << std::endl;
+  //   for (it = dict.begin(); it != dict.end(); it++) {
+  //     std::cout << it->first << ":" << it->second << ";" << std::endl;
+  //   }
+  //   std::cout << "----------" << std::endl;
+  // }
+  // return 0;
   Logger logger;
   ConnectionManager main_connection(&config, &logger);
   main_connection.setup();
+  main_connection.print_connection_manager();
+  std::cout << "Running server" << std::endl;
   main_connection.run();
 
   return(0);
