8951d192a2154298dad187695a5f4a6ed1e13d1e
timkingteam <timkingteam@gmail.com>
2024-10-22 01:10:04 +0200

    get_responses_string (not finished request not working yet)

--- src/execution/HttpHandle.hpp
+++ src/execution/HttpHandle.hpp
@@ -7,48 +7,53 @@
 #include <variant>
 #include <future>
 #include <filesystem>
+#include <HttpConnection.hpp>
 
-struct normalResponse {
-  std::string response;
-  bool is_keep_alive;
-};
+std::string get_responses_string(HttpConnection &connection);
+namespace HttpHandle {
 
-struct cgiResponse {
-  int cgi_pid;
-  int cgi_pipe[2];
-  bool is_keep_alive;
-};
+  struct normalResponse {
+    std::string response;
+    bool is_keep_alive;
+  };
 
-struct requestNotFinished {
-  bool is_chunked;
-};
+  struct cgiResponse {
+    int cgi_pid;
+    int cgi_pipe[2];
+    bool is_keep_alive;
+  };
 
-using response = std::variant<normalResponse, cgiResponse, requestNotFinished>;
+  struct requestNotFinished {
+    bool is_chunked_transfer;
+  };
 
-class HttpHandle {
-  public:
-    HttpHandle() = delete;
-    static response compose_response(const std::string& request_str, Config& config);
-  private:
-    static Config select_server_config(const HttpRequest& request, Config& config); // determines server config based on port and host. throws -> 404
-    static Config select_url_config(const std::string& url, Config& server_config); // selects config matching the url. throws -> 404
+  using response = std::variant<normalResponse, cgiResponse, requestNotFinished>;
 
-    enum class ContentType {
-      plain,
-      html,
-      css,
-    };
-    static std::string ok_response_head(ContentType t);
-    static response file_response(const std::string& file_path, Config& server_config, bool is_keep_alive);
-    static response redirection_response(const std::string& redirection_url, bool is_keep_alive);
-    static response directory_listing_response(const std::filesystem::path& directory_path, const std::string& url, bool is_keep_alive);
-    static response no_directory_listing_response(const std::filesystem::path& directory_path, Config& url_config, Config& server_config, bool is_keep_alive);
-    static std::string compose_object_path(const std::string& url, const std::string& server_url, const std::string& root);
-    static response execute_cgi_response(std::string scipt_path, std::string arg, bool is_keep_alive);
-    // static void run_cgi(std::promise<std::string>&& cgi_promise, const std::string& script_path, const std::string& arg); // TODO
-    static response status_code_to_response(int status_code, Config& server_config, bool is_keep_alive);
-    static response delete_file_response(const std::string& url_path, bool is_keep_alive);
+  class HttpHandle {
+    public:
+      HttpHandle() = delete;
+      static response compose_response(const std::string& request_str, Config& config);
+    private:
+      static Config select_server_config(const HttpRequest& request, Config& config); // determines server config based on port and host. throws -> 404
+      static Config select_url_config(const std::string& url, Config& server_config); // selects config matching the url. throws -> 404
 
-    // defined in DirectoryListing.cpp
-    static std::string directory_listing_html(const std::string& root_path, const std::vector<std::string>& leafs);
-};
+      enum class ContentType {
+        plain,
+        html,
+        css,
+      };
+      static std::string ok_response_head(ContentType t);
+      static response file_response(const std::string& file_path, Config& server_config, bool is_keep_alive);
+      static response redirection_response(const std::string& redirection_url, bool is_keep_alive);
+      static response directory_listing_response(const std::filesystem::path& directory_path, const std::string& url, bool is_keep_alive);
+      static response no_directory_listing_response(const std::filesystem::path& directory_path, Config& url_config, Config& server_config, bool is_keep_alive);
+      static std::string compose_object_path(const std::string& url, const std::string& server_url, const std::string& root);
+      static response execute_cgi_response(std::string scipt_path, std::string arg, bool is_keep_alive);
+      // static void run_cgi(std::promise<std::string>&& cgi_promise, const std::string& script_path, const std::string& arg); // TODO
+      static response status_code_to_response(int status_code, Config& server_config, bool is_keep_alive);
+      static response delete_file_response(const std::string& url_path, bool is_keep_alive);
+
+      // defined in DirectoryListing.cpp
+      static std::string directory_listing_html(const std::string& root_path, const std::vector<std::string>& leafs);
+  };
+}
--- src/execution/directory_listing/directory_listing_generator.py
+++ src/execution/directory_listing/directory_listing_generator.py
@@ -6,22 +6,24 @@ cpp = r'''#include <string>
 #include <assert.h>
 #include "HttpHandle.hpp"
 
-std::string HttpHandle::directory_listing_html(const std::string& root_path, const std::vector<std::string>& leafs) {
-    std::string html = R"(HTML_HERE)";
-    const std::string root_placeholded = "<!--root-->";
-    const std::string leaf_placeholded = "<!--leaf-->";
-    assert(html.find(root_placeholded) != html.size());
-    assert(html.find(leaf_placeholded) != html.size());
-    const std::string root_str = "<div class=\"root\">Index of " + root_path + "</div>";
-    html.replace(html.find(root_placeholded), root_placeholded.size(), root_str);
-    auto make_leaf = [](const std::string& leaf) {return "<div class=\"leaf\"><a href=\"" + leaf + "\">" + leaf + "</a></div>\n";};
-    std::string leaf_str;
-    for (const std::string& leaf : leafs) {
-        leaf_str += make_leaf(leaf);
+namespace HttpHandle {
+    std::string HttpHandle::directory_listing_html(const std::string& root_path, const std::vector<std::string>& leafs) {
+        std::string html = R"(HTML_HERE)";
+        const std::string root_placeholded = "<!--root-->";
+        const std::string leaf_placeholded = "<!--leaf-->";
+        assert(html.find(root_placeholded) != html.size());
+        assert(html.find(leaf_placeholded) != html.size());
+        const std::string root_str = "<div class=\"root\">Index of " + root_path + "</div>";
+        html.replace(html.find(root_placeholded), root_placeholded.size(), root_str);
+        auto make_leaf = [](const std::string& leaf) {return "<div class=\"leaf\"><a href=\"" + leaf + "\">" + leaf + "</a></div>\n";};
+        std::string leaf_str;
+        for (const std::string& leaf : leafs) {
+            leaf_str += make_leaf(leaf);
+        }
+        html.replace(html.find(leaf_placeholded), leaf_placeholded.size(), leaf_str);
+        return html;
     }
-    html.replace(html.find(leaf_placeholded), leaf_placeholded.size(), leaf_str);
-    return html;
-}
+};
 '''
 
 cpp = cpp.replace("HTML_HERE", template)
